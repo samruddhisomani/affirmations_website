87af6b6a0600eac071dc62198d2391b8a49983fe
Samruddhi Somani <sammysamhy@gmail.com>
2021-10-30 23:37:43 -0500

    Add affirmations fetch

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
-let hue = Math.random() * 360;
-let color = {
-  mode: "lchuv",
-  l: 90,
-  c: 20,
-  h: hue,
-  alpha: 1,
+const refresh = () => {
+  let hue = Math.random() * 360;
+  let color = {
+    mode: "lchuv",
+    l: 90,
+    c: 20,
+    h: hue,
+    alpha: 1,
+  };
+
+  document.getElementById("card").style.backgroundColor =
+    culori.formatHex8(color);
+
+  fetch("https://www.affirmations.dev/")
+    .then((response) => response.json())
+    .then(
+      (data) =>
+        (document.getElementById("affirmation").innerText = data.affirmation)
+    );
 };
-document.getElementById("card").style.backgroundColor =
-  culori.formatHex8(color);
